a0dbb976915551160f885ac304b4d50a851de751
rayruicai <58578046+rayruicai@users.noreply.github.com>
2020-01-06 17:52:00 -0800

    Create insertion-sort.py

---
+++ sorting-and-searching/insertion-sort.py
@@ -0,0 +1,22 @@
+import unittest
+
+# time complexity O(n**2)
+# space complexity O(1)
+
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1,n):
+        j = i - 1
+        while (j >= 0) and (arr[j] > arr[j+1]):
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+            j -= 1
+
+    return arr
+
+class Test(unittest.TestCase):
+    def test_insertion_sort(self):
+        arr = [3,6,9,7,8,4,2,5,1,9,6]
+        self.assertEqual(insertion_sort(arr), [1,2,3,4,5,6,6,7,8,9,9]);
+
+if __name__ == "__main__":
+    unittest.main()

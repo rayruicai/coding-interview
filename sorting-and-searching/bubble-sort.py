fc03f22ba5d75c1a3bd728fc75098b5d9eba4125
rayruicai <58578046+rayruicai@users.noreply.github.com>
2020-01-06 16:11:41 -0800

    Create bubble-sort.py

---
+++ sorting-and-searching/bubble-sort.py
@@ -0,0 +1,22 @@
+import unittest
+
+# time complexity O(n**2)
+# space complexity O(1)
+
+def bubble_sort(arr):
+    length = len(arr)
+    while length >= 2:
+        for i in range(length-1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+        length -= 1
+
+    return arr
+
+class Test(unittest.TestCase):
+    def test_bubble_sort(self):
+        arr = [3,6,9,7,8,4,2,5,1,9,6]
+        self.assertEqual(bubble_sort(arr), [1,2,3,4,5,6,6,7,8,9,9]);
+
+if __name__ == "__main__":
+    unittest.main()
